f148cc1349f240e607b7d9be27e9064c3767c626
Volen <vo13nn@gmail.com>
2017-07-06 21:54:16 +0300

    Extract calculating factorial for loop in method.

--- 02.CalculateFactorial/CalculateFactorialUnoptimized.cs
+++ 02.CalculateFactorial/CalculateFactorialUnoptimized.cs
@@ -10,22 +10,28 @@ namespace _02.CalculateFactorial
         static void Main()
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
-            int factorial = 1;
+            int factorial = 0;
             Random rand = new Random();
             int num;
             
             for (int i = 0; i < operationsCount; i++)
             {
                 num = rand.Next(1, 11);
-                for (int k = 1; k <= num; k++)
-                {
-                    factorial *= k;
-                    
-                }
-                factorial = 1;
+                factorial = CalculateFactorial(num);
             }
             stopwatch.Stop();
             Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        private static int CalculateFactorial(int num)
+        {
+            int factorial = 1;
+            for (int k = 1; k <= num; k++)
+            {
+                factorial *= k;
+            }
+
+            return factorial;
         }
     }
 }
